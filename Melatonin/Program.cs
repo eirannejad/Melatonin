12fead3b0d3fc8f2a9eb495e5cd400d73b3241b2
Ehsan Iran-Nejad <eirannejad@gmail.com>
2019-12-23 18:00:02 -0800

    First working draft

---
+++ Melatonin/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Forms;
+
+namespace Melatonin {
+    class Program {
+        static void Main(string[] args) {
+            // put machine to sleep
+            // source: https://stackoverflow.com/a/2080083
+            Application.SetSuspendState(PowerState.Suspend, true, true);
+        }
+    }
+}
---
+++ Melatonin/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Melatonin")]
+[assembly: AssemblyDescription("Put Windows to Sleep with a Single Click")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Melatonin")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8878997a-62b0-4fd3-b5d4-7e0f39e307ca")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
